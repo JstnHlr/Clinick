d4056c9086ef7af32f4e21a807a9ba5ee49fe552
JstnHlr <102547135+JstnHlr@users.noreply.github.com>
2022-05-02 18:54:56 +0800

    Delete loginwindow.js

--- loginwindow.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("#show-signin").addEventListener("click", function() {
-    document.querySelector(".pop-up").classList.add("active");
-});
-document.querySelector(".pop-up .close-btn").addEventListener("click", function() {
-    document.querySelector(".pop-up").classList.remove("active");
-});
